5a5da4f2da5e9bf572a0f655703b4b9c7d0bf74d
Andrew Cline <andrew.cline77@gmail.com>
2021-08-25 10:40:28 -0500

    feat: add basic get book query

--- src/books/queries.ts
+++ src/books/queries.ts
@@ -33,3 +33,37 @@ export const listBooks = async (): Promise<ListBooksQuery> => {
 
   return jsonResult
 }
+
+type GetBookQuery = {
+  data: {
+    findBookByID: Book
+  }
+  errors: any[]
+}
+
+export const getBook = async (): Promise<GetBookQuery> => {
+  const GET_BOOK_QUERY = `
+    query GetBook($id:ID!) { 
+      findBookByID(id: ID!) { 
+        _id 
+        title 
+        description 
+        author
+      } 
+    }
+  `
+
+  const fetchResult = await fetch(process.env.VUE_APP_GRAPHQL_URL, {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      authorization: 'Basic ' + process.env.VUE_APP_FAUNA_SECERT,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ query: GET_BOOK_QUERY })
+  })
+
+  const jsonResult = await fetchResult.json()
+
+  return jsonResult
+}
