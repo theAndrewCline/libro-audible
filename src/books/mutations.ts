e955a2a6c81fdc36d6adbcfdf4475ef71330bec8
Andrew Cline <andrew.cline77@gmail.com>
2021-08-25 10:52:47 -0500

    feat: add missing mutations

--- src/books/mutations.ts
+++ src/books/mutations.ts
@@ -43,3 +43,88 @@ export const createBook = async (
 
   return result
 }
+
+type UpdateBookInput = {
+  _id: string
+  title?: string
+  description?: string
+  author?: string
+}
+
+type UpdateBookResponse = {
+  data: {
+    updateBook: {
+      _id: string
+      title: string
+      description: string
+      author: string
+    }
+  }
+  errors: any[]
+}
+
+const UPDATE_BOOK = `
+  mutation UpdateBook($id: ID!, $data: BookInput!) {
+    updateBook(id:$id, data:$data) {
+      _id
+      title
+      description
+      author
+    }
+  }
+`
+
+export const updateBook = async (
+  variables: UpdateBookInput
+): Promise<UpdateBookResponse> => {
+  const fetchResult = await fetch(process.env.VUE_APP_GRAPHQL_URL, {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      authorization: 'Basic ' + process.env.VUE_APP_FAUNA_SECRET,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ query: UPDATE_BOOK, variables })
+  })
+
+  const result = await fetchResult.json()
+
+  return result
+}
+
+type DeleteBookInput = string
+
+type DeleteBookResponse = {
+  data: {
+    deleteBook: {
+      _id: string
+    }
+  }
+  errors: any[]
+}
+
+const DELETE_BOOK = `
+  mutation DeleteBook($id: ID!) {
+    deleteBook(id:$id) {
+      _id
+    }
+  }
+`
+
+export const deleteBook = async (
+  id: DeleteBookInput
+): Promise<DeleteBookResponse> => {
+  const fetchResult = await fetch(process.env.VUE_APP_GRAPHQL_URL, {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      authorization: 'Basic ' + process.env.VUE_APP_FAUNA_SECRET,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ query: DELETE_BOOK, variables: { id } })
+  })
+
+  const result = await fetchResult.json()
+
+  return result
+}
